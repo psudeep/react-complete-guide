e44912a34e6d32ea75d456bdeec7697f467a02e9
Prashant Sudeep <prashant.sudeep89@gmail.com>
2020-04-06 14:55:32 +0530

    ceatElement example

--- src/App.js
+++ src/App.js
@@ -4,18 +4,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        {/*<header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>*/}
-        <h1>Hi, I am a React App</h1>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <h1>Hi, I am a React App</h1>
+    //   </div>
+    // );
+    return React.createElement('div', null, 'h1', 'Hi I am a React App !!');
   }
 }
 
