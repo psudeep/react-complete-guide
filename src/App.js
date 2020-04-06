3d17bc0c598ef2070592a21cc4cd7ea9e052ee39
Prashant Sudeep <prashant.sudeep89@gmail.com>
2020-04-06 15:11:46 +0530

    packaages install and new component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
+        <p>This is really work</p>
+        <Person />
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now ??'));
---
+++ src/Person/Person.js
@@ -0,0 +1,8 @@
+//a componet is a function which return jsx/dom example fo functional component
+import React from 'react';
+
+const person = () => {
+	return <p>I am a person !</p>
+};
+
+export default person;
