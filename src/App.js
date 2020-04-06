4d2c578ee66b0e55274d9e4359ce4cae96a70c4f
Prashant Sudeep <prashant.sudeep89@gmail.com>
2020-04-06 15:31:53 +0530

    using state in Class Component and changing value using state property

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  //state is mananged from inside a component which extends Component class
+  state = {
+    persons: [
+      {name: 'Prashant', age: '29'},
+      {name: 'Sipu', age: '31'},
+      {name: 'Sahiba', age: '28'},
+    ],
+    isAuthenticated: false,
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <p>This is really work</p>
-        <Person name="Prashant" age="29" />
-        <Person name="Sipu" age="31">is racing</Person>
+        <button>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>is racing</Person>
         <Person name="Sahiba" age="28" />
       </div>
     );
