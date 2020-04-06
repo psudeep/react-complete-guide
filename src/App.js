64cc3301514a1247ee4557580f07c6777b43b931
Prashant Sudeep <prashant.sudeep89@gmail.com>
2020-04-06 15:21:21 +0530

    dynamic content using props

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <p>This is really work</p>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Prashant" age="29" />
+        <Person name="Sipu" age="31">is racing</Person>
+        <Person name="Sahiba" age="28" />
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now ??'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,8 @@
 //a componet is a function which return jsx/dom example fo functional component
 import React from 'react';
 
-const person = () => {
-	return <p>I am a person and I am {Math.floor(Math.random() *30)} years old!</p>
+const person = (props) => {
+	return <p>I am a {props.name} and I am {props.age} years old!</p>
 };
 
 export default person;
