78d05f98fa218555c97dda7423020f1c01d00f24
Prashant Sudeep <prashant.sudeep89@gmail.com>
2020-04-06 17:07:31 +0530

    hooks added for state management in react

--- src/App.js
+++ src/App.js
@@ -1,44 +1,61 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
+// class App extends Component {
 
-  //state is mananged from inside a component which extends Component class
-  state = {
+const app = (props) => {
+
+  const [personsState, setPersonsState] = useState({
     persons: [
       {name: 'Prashant', age: '29'},
       {name: 'Sipu', age: '31'},
       {name: 'Sahiba', age: '28'},
     ],
-    isAuthenticated: false,
-  };
+    //isAuthenticated: false,
+  });
 
-  switchNameHandler = () => {
+  const [isAuthenticatedState, setAuthState] = useState({
+    isAuthenticated: true,
+  });
+
+  console.log(personsState, isAuthenticatedState);
+
+  const switchNameHandler = () => {
     console.log('was clicked');
     // DOM't DO THIS this.state.persons[0] = 'Prashant Sudeep';
-    this.setState({
+    setPersonsState({
       persons: [
         {name: 'Prashant Sudeep', age: '29'},
         {name: 'Sipu', age: '31'},
         {name: 'Sahiba', age: '24'},
-      ]
+      ],
+      isAuthenticated: isAuthenticatedState.isAuthenticated,
     });
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I am a React App</h1>
-        <p>This is really work</p>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>is racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
-    );
-    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now ??'));
-  }
+  //state is mananged from inside a component which extends Component class
+  
+  return (
+    <div className="App">
+      <h1>Hi, I am a React App</h1>
+      <p>This is really work</p>
+      <button onClick={switchNameHandler}>Switch name</button>
+      <Person 
+        name={personsState.persons[0].name} 
+        age={personsState.persons[0].age} 
+      />
+      <Person 
+        name={personsState.persons[1].name} 
+        age={personsState.persons[1].age}>
+          is racing
+        </Person>
+      <Person 
+        name={personsState.persons[2].name} 
+        age={personsState.persons[2].age} 
+      />
+    </div>
+  );
 }
 
-export default App;
+export default app;
