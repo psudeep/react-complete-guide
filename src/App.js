dfaf26aa5c0f04905bb669e093d8d530fd5c8cc6
Prashant Sudeep <prashant.sudeep89@gmail.com>
2020-04-06 17:16:20 +0530

    class based component used now

--- src/App.js
+++ src/App.js
@@ -29,13 +29,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I am a React App</h1>
-        <p>This is really work</p>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>is racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
+      <h1>Hi, I am a React App</h1>
+      <p>This is really work</p>
+      <button onClick={this.switchNameHandler}>Switch name</button>
+      <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age} 
+      />
+      <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}>
+          is racing
+        </Person>
+      <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age} 
+      />
+    </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now ??'));
   }
