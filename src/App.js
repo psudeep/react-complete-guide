c5cedffa0bf042d1bcbb74b053a2082c5e63d89b
Prashant Sudeep <prashant.sudeep89@gmail.com>
2020-04-06 15:00:31 +0530

    createElement back to start

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I am a React App</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now ??'));
+    return (
+      <div className="App">
+        <h1>Hi, I am a React App</h1>
+      </div>
+    );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now ??'));
   }
 }
 
